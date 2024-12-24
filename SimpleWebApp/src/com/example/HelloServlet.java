877d4bdd6010dbfd93e0a9a0c5e3bbef9da5bbef
sai kenguva <skenguva2@gmail.com>
2024-12-24 14:42:17 +0000

    sample web app

---
+++ SimpleWebApp/src/com/example/HelloServlet.java
@@ -0,0 +1,21 @@
+ipackage com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, " + name + "!</h1>");
+    }
+}
+
